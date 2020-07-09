4f074089ebe77a54c6d597c509ea88ae3d36ad99
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-07-09 20:05:12 -0300

    add binary

---
+++ src/1d/binary.ts
@@ -0,0 +1,7 @@
+import { UnitFunction1d } from './UnitFunction1d';
+import { Unit } from '../Unit';
+import { threshold } from '../2d/threshold';
+
+export const binary: UnitFunction1d = t => {
+  return threshold(0.5 as Unit, t);
+};
