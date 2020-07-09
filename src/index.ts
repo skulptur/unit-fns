0ae07cce5540cca9f9617a73c657f1b05b946b84
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-07-09 20:25:59 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
 export { Unit } from './Unit';
 export { toUnit } from './toUnit';
+export { unitMin } from './unitMin';
+export { unitMax } from './unitMax';
+export { mapUnit } from './mapUnit';
+export { phase } from './phase';
+export { repeat } from './repeat';
+export { fraction } from './fraction';
+
+// 1d
+export { binary } from './1d/binary';
+export { inverse } from './1d/inverse';
+export { linear } from './1d/linear';
+
+// 2d
+export { peak } from './2d/peak';
+export { threshold } from './2d/threshold';
+
+// 3d
+export { linearInterpolation } from './3d/linearInterpolation';
