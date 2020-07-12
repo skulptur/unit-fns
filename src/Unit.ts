0a922e73b0734242d30cf9ae069c20f2c7c8f4b9
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-07-12 01:32:44 -0300

    fix error building

--- src/Unit.ts
+++ src/Unit.ts
@@ -3,6 +3,6 @@ class Opaque<T> {
   private __TYPE__: T;
 }
 
-type Nominal<Tag extends string, Type> = Type & Opaque<Tag>;
+export type Nominal<Tag extends string, Type> = Type & Opaque<Tag>;
 
 export type Unit = Nominal<'Unit', number>;
