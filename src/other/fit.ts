1eed1815ba0e859da3f05738ac301068c6d512ba
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2020-08-22 16:43:04 -0300

    remove UnitFunction1d from fit fn

--- src/other/fit.ts
+++ src/other/fit.ts
@@ -1,10 +1,12 @@
-import { UnitFunction1d } from '../1d/UnitFunction1d'
 import { Unit } from '../core/Unit'
 import { repeat } from '../2d/repeat'
 import { subdivision } from '../number/subdivision'
 import { toIndex } from '../number/toIndex'
 
-export const fit = (fns: ReadonlyArray<UnitFunction1d>, unit: Unit): Unit => {
+export const fit = (
+  fns: ReadonlyArray<(a: Unit) => Unit>,
+  unit: Unit
+): Unit => {
   const fnsLength = fns.length
   const t = repeat(subdivision(fnsLength), unit)
   const fn = fns[toIndex(fnsLength, unit)]
