87240d9e4bdc3940957b1a752c6fe0e102367e28
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2022-03-06 21:45:17 -0300

    implement url navigation

--- example/index.tsx
+++ example/index.tsx
@@ -3,20 +3,40 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { ImageData } from './src/components/ImageData'
 import * as sketchMap from './src/graphics'
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
 
-// TODO: use url param to select sketch
+const sketches = Object.entries(sketchMap)
+const clamp = (num: number, min: number, max: number) => {
+  return Math.min(Math.max(num, min), max)
+}
+
 const App = () => {
+  const params = useParams<{ id: string }>()
+  const currentId = parseInt(params.id || '0', 10)
+  const [sketchId, onSample] = sketches[currentId]
+
+  const onNavigate = (targetId: number) => () => {
+    const redirectId = clamp(targetId, 0, sketches.length - 1)
+    if (redirectId === currentId) return
+    location.href = `${location.origin}/${redirectId}`
+  }
+
   return (
     <div>
-      {Object.entries(sketchMap).map(([name, onSample]) => {
-        return (
-          <div key={name}>
-            <ImageData width={1000} height={1000} onSample={onSample} />
-          </div>
-        )
-      })}
+      <button onClick={onNavigate(currentId - 1)}>previous</button>
+      <button onClick={onNavigate(currentId + 1)}>next</button>
+      <div key={sketchId}>
+        <ImageData width={1000} height={1000} onSample={onSample} />
+      </div>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/:id" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
