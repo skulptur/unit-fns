c7b5c309c38f6d7356819b843de104b1de2754fb
Gustavo Spredemann <g.spredemann@gmail.com>
2022-04-29 19:01:13 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import * as _unary from './lib/unary'
 import * as _binary from './lib/binary'
 import * as _ternary from './lib/ternary'
+export const unary = _unary
+export const binary = _binary
+export const ternary = _ternary
 
 export * from './lib/core'
 export * from './lib/unary'
-
-export const unary = _unary
 export * from './lib/binary'
-
-export const binary = _binary
 export * from './lib/ternary'
-
-export const ternary = _ternary
 export * from './lib/number'
 export * from './lib/other'
