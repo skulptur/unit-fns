0c30833445f80b3dd1fcd8c9df61b9953d6099ce
Gustavo Spredemann <g.spredemann@gmail.com>
2022-04-30 12:40:58 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,9 @@ export * from './lib/binary'
 export * from './lib/ternary'
 export * from './lib/number'
 export * from './lib/other'
+
+// TODO:
+// https://iquilezles.org/articles/functions/
+// http://www.flong.com/archive/texts/code/shapers_poly/
+// http://www.flong.com/archive/texts/code/shapers_exp/
+// http://www.flong.com/archive/texts/code/shapers_circ/
